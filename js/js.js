d479df86d1a8387277dc110dd24783cde94bba59
EJefferyCode <emmyjeffery99@gmail.com>
2017-01-11 19:13:55 -0500

    First commit

---
+++ js/js.js
@@ -0,0 +1,4 @@
+var years = prompt('Enter your age');
+var name = prompt('Enter your name');
+var city = prompt('Enter your city');
+document.write('Your name is ' +name+ ','+ ' ' + 'you are ' +years+ ' ' + 'years old, and you live in ' +city);
